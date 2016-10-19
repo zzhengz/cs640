7234e62874a20c43af0a606bb3739f7f31449819
zzhengz <zzheng45@wisc.edu>
2016-10-19 13:49:07 -0500

    updated customized tests

--- test_lru.py
+++ test_lru.py
@@ -35,20 +35,24 @@ def hub_tests():
     macP3 = '10:00:00:00:00:04'
     macP4 = '10:00:00:00:00:05'
     macP5 = '10:00:00:00:00:06'
-    macH1 = '20:00:00:00:00:01'
-    macH2 = '30:00:00:00:00:01'
-    macH3 = '40:00:00:00:00:01'
-    macH4 = '50:00:00:00:00:01'
-    macH5 = '60:00:00:00:00:01'
-    macH6 = '70:00:00:00:00:01'
+    macH2 = '20:00:00:00:00:01'
+    macH3 = '30:00:00:00:00:01'
+    macH4 = '40:00:00:00:00:01'
+    macH5 = '50:00:00:00:00:01'
+    macH6 = '60:00:00:00:00:01'
+    macH7 = '70:00:00:00:00:01'
+    macH8 = '80:00:00:00:00:01'
+    macH9 = '90:00:00:00:00:01'
     macAddrBroadcast = 'ff:ff:ff:ff:ff:ff'
 
-    ipH1 = '192.168.100.1'
     ipH2 = '192.168.100.2'
     ipH3 = '192.168.100.3'
     ipH4 = '192.168.100.4'
     ipH5 = '192.168.100.5'
     ipH6 = '192.168.100.6'
+    ipH7 = '192.168.100.7'
+    ipH8 = '192.168.100.8'
+    ipH9 = '192.168.100.9'
 
     s = Scenario("LRU switch tests")
     s.add_interface('eth0', macP0)
@@ -65,68 +69,96 @@ def hub_tests():
 
     # test case 1: a frame with broadcast destination should get sent out
     # all ports except ingress
-    testpkt = mk_pkt(macH2, macAddrBroadcast, ipH2, "255.255.255.255")
+    testpkt = mk_pkt(macH3, macAddrBroadcast, ipH3, "255.255.255.255")
     s.expect(PacketInputEvent("eth1", testpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame with a broadcast destination address should arrive on eth1")
-    s.expect(PacketOutputEvent("eth0", testpkt, "eth2", testpkt, "eth3", testpkt, "eth4", testpkt, "eth5", testpkt, display=Ethernet), "Basic_switch_test: The Ethernet frame with a broadcast destination address should be forwarded out ports eth0 and eth2")
+    s.expect(PacketOutputEvent("eth0", testpkt, "eth2", testpkt, "eth3", testpkt, "eth4", testpkt, "eth5", testpkt, display=Ethernet), "Basic_switch_test: The Ethernet frame with a broadcast destination address should be forwarded out ports eth0 and eth2, lru=[3 -> eth1]")
 
     # test case 2: a frame with any unicast address except one assigned to hub
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)
-    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH1 to macH2 should arrive on eth0")
-    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2)") 
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH2 to macH3 should arrive on eth0")
+    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2), lru=[2 -> eth0,3 -> eth1])") 
 
-    resppkt = mk_pkt(macH2, macH1, ipH2, '192.168.1.100', reply=True)
-    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH1 should arrive on eth1")
-    s.expect(PacketOutputEvent("eth0", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth0")
+    resppkt = mk_pkt(macH3, macH2, ipH3, '192.168.1.100', reply=True)
+    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH2 should arrive on eth1")
+    s.expect(PacketOutputEvent("eth0", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth0, lru=[3 -> eth1, 2 -> eth0]")
 
     # test case 3: a frame with dest address of one of the interfaces should
     # result in nothing happening
-    reqpkt = mk_pkt(macH1, macP2, ipH1,'172.16.42.2')
+    reqpkt = mk_pkt(macH2, macP2, ipH2,'172.16.42.2')
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame should arrive on eth0 with destination address the same as eth0's MAC address")
-    s.expect(PacketInputTimeoutEvent(1.0), "Basic_switch_test: The switch should not do anything in response to a frame arriving with a destination address referring to the switch itself.")
+    s.expect(PacketInputTimeoutEvent(1.0), "Basic_switch_test: The switch should not do anything in response to a frame arriving with a destination address referring to the switch itself, lru=[3 -> eth1, 2 -> eth0]")
 
 
     # test case 4: host1 reconnected to eth2, switch should know this topology change
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)
-    s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH1 to macH2 should arrive on eth0")
-    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2)") 
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)
+    s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH2 to macH3 should arrive on eth0")
+    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2), lru=[ 2 -> eth2, 3 -> eth1]") 
 
-    resppkt = mk_pkt(macH2, macH1, ipH2, ipH1, reply=True)
-    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH1 should arrive on eth1")
-    s.expect(PacketOutputEvent("eth2", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth2")
+    resppkt = mk_pkt(macH3, macH2, ipH3, ipH2, reply=True)
+    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH2 should arrive on eth1")
+    s.expect(PacketOutputEvent("eth2", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth2, lru=[ 3 -> eth1,2 -> eth2]")
 
     # test case 5: host1 reconnected to eth0, switch should know this topology change
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "LRU_switch_test: [h1 h2]") 
+    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "LRU_switch_test: [h2 h3]") 
 
-    reqpkt = mk_pkt(macH3, macH1, ipH3, ipH1)
+    reqpkt = mk_pkt(macH4, macH2, ipH4, ipH2)
     s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h2 h3 h1]")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h3 h4 h2]")
 
-    reqpkt = mk_pkt(macH4, macH1, ipH4, ipH1)
+    reqpkt = mk_pkt(macH5, macH2, ipH5, ipH2)
     s.expect(PacketInputEvent("eth3", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h2 h3 h4 h1]")
-
-    reqpkt = mk_pkt(macH5, macH1, ipH5, ipH1)
-    s.expect(PacketInputEvent("eth4", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h2 h3 h4 h5 h1]")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h3 h4 h5 h2]")
 
     reqpkt = mk_pkt(macH6, macH2, ipH6, ipH2)
-    s.expect(PacketInputEvent("eth5", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent("eth0", reqpkt,"eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt, display=Ethernet), "LRU_switch_test: [h3 h4 h5 h1 h6]")
+    s.expect(PacketInputEvent("eth4", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: [h3 h4 h5 h6 h2]")
 
-    reqpkt = mk_pkt(macH1, macH3, ipH1, ipH3)
+    reqpkt = mk_pkt(macH7, macH3, ipH7, ipH2)
+    s.expect(PacketInputEvent("eth5", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketOutputEvent("eth0", reqpkt,"eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt, display=Ethernet), "LRU_switch_test: [h4 h5 h6 h2 h7]")
+
+    reqpkt = mk_pkt(macH2, macH4, ipH2, ipH4)
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent( "eth2", reqpkt, display=Ethernet), "LRU_switch_test: shoud be broadcast [h4 h5 h6 h1 h3]")
+    s.expect(PacketOutputEvent( "eth2", reqpkt, display=Ethernet), "LRU_switch_test: shoud be broadcast [h5 h6 h2 h7 h4]")
 
     # test case 6: 
-    reqpkt = mk_pkt(macH1, macP3, ipH1,'172.16.42.2')
+    reqpkt = mk_pkt(macH3, macP3, ipH3,'172.16.42.2')
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketInputTimeoutEvent(1.0), "LRU_switch_test: sent to switch port 3, should do nothing here, [h4 h5 h6 h3 h1]")
+    s.expect(PacketInputTimeoutEvent(1.0), "LRU_switch_test: sent to switch port 3, should do nothing here, [h6 h2 h7 h4 h3]")
 
-    reqpkt = mk_pkt(macH1, macH4, ipH1, ipH3)
+    reqpkt = mk_pkt(macH7, macH5, ipH7, ipH5)
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
-    s.expect(PacketOutputEvent( "eth3", reqpkt,  display=Ethernet), "LRU_switch_test: shoud be broadcast [h5 h6 h2 h1 h4]")
+    s.expect(PacketOutputEvent( "eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt,"eth5", reqpkt,  display=Ethernet), "LRU_switch_test: shoud be broadcast [h6 h2 h7 h4 h3]")
+
+    reqpkt = mk_pkt(macH8, macP3, ipH8,'172.16.42.2')
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketInputTimeoutEvent(1.0), "LRU_switch_test: sent to switch port 3, should do nothing here, [h2 h7 h4 h3 h8]")
+
+    reqpkt = mk_pkt(macH2, macH6, ipH2, ipH6)
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketOutputEvent( "eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt,"eth5", reqpkt,  display=Ethernet), "LRU_switch_test: shoud be broadcast [h2 h7 h4 h3 h8]")
+
+    reqpkt = mk_pkt(macH9, macP3, ipH9,'172.16.42.2')
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketInputTimeoutEvent(1.0), "LRU_switch_test: sent to switch port 3, should do nothing here, [h7 h4 h3 h8 h9]")
+
+    reqpkt = mk_pkt(macH7, macH2, ipH7, ipH2)
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketOutputEvent( "eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt,"eth5", reqpkt,  display=Ethernet), "LRU_switch_test: shoud be broadcast [h7 h4 h3 h8 h9]")
+
+    reqpkt = mk_pkt(macH2, macP3, ipH2,'172.16.42.2')
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketInputTimeoutEvent(1.0), "LRU_switch_test: sent to switch port 3, should do nothing here, [h4 h3 h8 h9 h2]")
+
+    reqpkt = mk_pkt(macH4, macH7, ipH4, ipH7)
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "LRU_switch_test: receive")
+    s.expect(PacketOutputEvent( "eth1", reqpkt,"eth2", reqpkt,"eth3", reqpkt,"eth4", reqpkt,"eth5", reqpkt,  display=Ethernet), "LRU_switch_test: shoud be broadcast [h4 h3 h8 h9 h2]")
+
+
+
+
 
     return s
 
--- test_traffic.py
+++ test_traffic.py
@@ -35,20 +35,20 @@ def hub_tests():
     macP3 = '10:00:00:00:00:04'
     macP4 = '10:00:00:00:00:05'
     macP5 = '10:00:00:00:00:06'
-    macH1 = '20:00:00:00:00:01'
-    macH2 = '30:00:00:00:00:01'
-    macH3 = '40:00:00:00:00:01'
-    macH4 = '50:00:00:00:00:01'
-    macH5 = '60:00:00:00:00:01'
-    macH6 = '70:00:00:00:00:01'
+    macH2 = '20:00:00:00:00:01'
+    macH3 = '30:00:00:00:00:01'
+    macH4 = '40:00:00:00:00:01'
+    macH5 = '50:00:00:00:00:01'
+    macH6 = '60:00:00:00:00:01'
+    macH7 = '70:00:00:00:00:01'
     macAddrBroadcast = 'ff:ff:ff:ff:ff:ff'
 
-    ipH1 = '192.168.100.1'
     ipH2 = '192.168.100.2'
     ipH3 = '192.168.100.3'
     ipH4 = '192.168.100.4'
     ipH5 = '192.168.100.5'
     ipH6 = '192.168.100.6'
+    ipH7 = '192.168.100.7'
 
     s = Scenario("LRU switch tests")
     s.add_interface('eth0', macP0)
@@ -65,67 +65,71 @@ def hub_tests():
 
     # test case 1: a frame with broadcast destination should get sent out
     # all ports except ingress
-    testpkt = mk_pkt(macH2, macAddrBroadcast, ipH2, "255.255.255.255")  #[h2:0]
+    testpkt = mk_pkt(macH3, macAddrBroadcast, ipH3, "255.255.255.255")  #[h3:0]
     s.expect(PacketInputEvent("eth1", testpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame with a broadcast destination address should arrive on eth1")
     s.expect(PacketOutputEvent("eth0", testpkt, "eth2", testpkt, "eth3", testpkt, "eth4", testpkt, "eth5", testpkt, display=Ethernet), "Basic_switch_test: The Ethernet frame with a broadcast destination address should be forwarded out ports eth0 and eth2")
 
     # test case 2: a frame with any unicast address except one assigned to hub
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)    #[h1:0 h2:1]
-    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH1 to macH2 should arrive on eth0")
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)    #[h2:0 h3:1]
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH2 to macH3 should arrive on eth0")
     s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2)") 
 
-    resppkt = mk_pkt(macH2, macH1, ipH2, '192.168.1.100', reply=True)    #[h1:1 h2:1]
-    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH1 should arrive on eth1")
+    resppkt = mk_pkt(macH3, macH2, ipH3, ipH2, reply=True)    #[h2:1 h3:1]
+    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH2 should arrive on eth1")
     s.expect(PacketOutputEvent("eth0", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth0")
 
     # test case 3: a frame with dest address of one of the interfaces should
     # result in nothing happening
-    reqpkt = mk_pkt(macH1, macP2, ipH1,'172.16.42.2')    #[h1:1 h2:1]
+    reqpkt = mk_pkt(macH2, macP2, ipH2,'172.16.42.2')    #[h2:1 h3:1]
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame should arrive on eth0 with destination address the same as eth0's MAC address")
     s.expect(PacketInputTimeoutEvent(1.0), "Basic_switch_test: The switch should not do anything in response to a frame arriving with a destination address referring to the switch itself.")
 
 
     # test case 4: host1 reconnected to eth2, switch should know this topology change
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)    #[h1:1 h2:2]
-    s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH1 to macH2 should arrive on eth0")
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)    #[h2:1 h3:2]
+    s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from macH2 to macH3 should arrive on eth0")
     s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined for 30:00:00:00:00:02 should be flooded out eth1(not eth2)") 
 
-    resppkt = mk_pkt(macH2, macH1, ipH2, ipH1, reply=True)    #[h1:2 h2:2]
-    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH1 should arrive on eth1")
+    resppkt = mk_pkt(macH3, macH2, ipH3, ipH2, reply=True)    #[h2:2 h3:2]
+    s.expect(PacketInputEvent("eth1", resppkt, display=Ethernet), "Basic_switch_test: An Ethernet frame from 30:00:00:00:00:01 to macH2 should arrive on eth1")
     s.expect(PacketOutputEvent("eth2", resppkt, display=Ethernet), "Basic_switch_test: Ethernet frame destined to 20:00:00:00:00:01 should be flooded out eth2")
 
     # test case 5: host1 reconnected to eth0, switch should know this topology change
-    reqpkt = mk_pkt(macH1, macH2, ipH1,ipH2)    #[h1:2 h2:3]
+    reqpkt = mk_pkt(macH2, macH3, ipH2,ipH3)    #[h2:2 h3:3]
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "traffic_switch_test: [h1:2 h2:3]") 
+    s.expect(PacketOutputEvent("eth1", reqpkt, display=Ethernet), "traffic_switch_test: [h2:2 h3:3]") 
 
-    reqpkt = mk_pkt(macH3, macH1, ipH3, ipH1)    #[h1:3 h2:3 h3:0]
+    reqpkt = mk_pkt(macH4, macH2, ipH4, ipH2)    #[h2:3 h3:3 h4:0]
     s.expect(PacketInputEvent("eth2", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h1:3 h2:3 h3:0]")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h2:3 h3:3 h4:0]")
 
-    reqpkt = mk_pkt(macH4, macH1, ipH4, ipH1)    #[h1:4 h2:3 h3:0 h4:0]
-    s.expect(PacketInputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h1:4 h2:3 h3:0 h4:0]")
-
-    reqpkt = mk_pkt(macH4, macH1, ipH4, ipH1)    #[h1:5 h2:3 h3:0 h4:0]
-    s.expect(PacketInputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h1:5 h2:3 h3:0 h4:1]")
-
-    reqpkt = mk_pkt(macH1, macH4, ipH4, ipH1)    #[h1:5 h2:3 h3:0 h4:1]
+    reqpkt = mk_pkt(macH2, macH4, ipH2, ipH4)    #[h2:3 h3:3 h4:1]
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: [h1:5 h2:3 h3:0 h4:1]")
+    s.expect(PacketOutputEvent("eth2", reqpkt, display=Ethernet), "traffic_switch_test: [h2:3 h3:3 h4:1]")
 
-    reqpkt = mk_pkt(macH5, macH1, ipH5, ipH1)    #[h1:6 h2:3 h3:0 h4:1 h5:0]
+    reqpkt = mk_pkt(macH5, macH2, ipH5, ipH2)    #[h2:4 h3:3 h4:1 h5:0]
+    s.expect(PacketInputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: ")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h2:4 h3:3 h4:1 h5:0]")
+
+    reqpkt = mk_pkt(macH5, macH2, ipH5, ipH2)    #[h2:5 h3:3 h4:1 h5:0]
+    s.expect(PacketInputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: ")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h2:5 h3:3 h4:1 h5:0]")
+
+    reqpkt = mk_pkt(macH2, macH5, ipH2, ipH5)    #[h2:5 h3:3 h4:1 h5:1]
+    s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: ")
+    s.expect(PacketOutputEvent("eth3", reqpkt, display=Ethernet), "traffic_switch_test: [h2:5 h3:3 h4:1 h5:1]")
+
+    reqpkt = mk_pkt(macH6, macH2, ipH6, ipH2)    #[h2:6 h3:3 h4:1 h5:1 h6:0]
     s.expect(PacketInputEvent("eth4", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h1:6 h2:3 h3:0 h4:1 h5:0]")
+    s.expect(PacketOutputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: [h2:6 h3:3 h4:1 h5:1 h6:0]")
 
-    reqpkt = mk_pkt( macH6, macH5, ipH1, ipH3)    #[h1:7 h2:3 h3:1 h4:1 h6:0]
+    reqpkt = mk_pkt( macH7, macH6, ipH7, ipH6)    #[h2:7 h3:3 h4:1 h5:1 h7:0]
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth1", reqpkt, "eth2", reqpkt, "eth3", reqpkt, "eth4", reqpkt, "eth5", reqpkt, display=Ethernet), "broadcast traffic_switch_test: [h1:7 h2:3 h3:1 h4:1 h6:0]")
+    s.expect(PacketOutputEvent("eth1", reqpkt, "eth2", reqpkt, "eth3", reqpkt, "eth4", reqpkt, "eth5", reqpkt, display=Ethernet), "broadcast traffic_switch_test: [h2:7 h3:3 h4:1 h5:1 h7:0]")
 
-    reqpkt = mk_pkt( macH1, macH5, ipH1, ipH3)    #[h1:7 h2:3 h3:1 h4:1 h6:0]
+    reqpkt = mk_pkt( macH2, macH6, ipH2, ipH6)    #[h2:7 h3:3 h4:1 h5:1 h7:0]
     s.expect(PacketInputEvent("eth0", reqpkt, display=Ethernet), "traffic_switch_test: ")
-    s.expect(PacketOutputEvent("eth1", reqpkt, "eth2", reqpkt, "eth3", reqpkt, "eth4", reqpkt, "eth5", reqpkt, display=Ethernet), "broadcast traffic_switch_test: [h1:7 h2:3 h3:1 h4:1 h5:0]")
+    s.expect(PacketOutputEvent("eth1", reqpkt, "eth2", reqpkt, "eth3", reqpkt, "eth4", reqpkt, "eth5", reqpkt, display=Ethernet), "broadcast traffic_switch_test: [h2:7 h3:3 h4:1 h5:1 h7:0]")
 
 
     return s
